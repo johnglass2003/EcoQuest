fafc0ff0878058bb498b48f38432c6132491dc20
John Glass <50562358+johnglass2003@users.noreply.github.com>
2025-01-25 20:30:59 -0500

    CLEEEEAN SLAAATE

--- EcoQuest/src/app/app.module.ts
+++ EcoQuest/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp({ projectId: "ecoquest-97df5", appId: "1:441011459321:web:06c967fdc599b9f2003e28", storageBucket: "ecoquest-97df5.firebasestorage.app", apiKey: "AIzaSyCiaQGwRql8lntN5smYbKPQ0H-hTIwvzEA", authDomain: "ecoquest-97df5.firebaseapp.com", messagingSenderId: "441011459321", measurementId: "G-5V50LXYN91" })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   bootstrap: [AppComponent]
 })
